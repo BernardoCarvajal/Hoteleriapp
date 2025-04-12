172ccc2e4f1a493084916701b04dc5a0ca4965fa
BernardoCarvajal <163200059+BernardoCarvajal@users.noreply.github.com>
2025-04-11 23:24:23 -0400

    Se permite CORS de 4200 de Angular

--- BACKEND/app/api.py
+++ BACKEND/app/api.py
@@ -35,6 +35,8 @@ def create_app() -> FastAPI:
         "http://127.0.0.1:8000",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+        "http://localhost:4200",
+        "http://127.0.0.1:4200",
         # Añade aquí otros orígenes que necesites permitir
     ]
 
