c3c16d5c0531ad245bfcd308c6a88c596e0bbb59
BernardoCarvajal <163200059+BernardoCarvajal@users.noreply.github.com>
2025-04-27 13:39:28 -0400

    Back de rutas

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -1,10 +1,11 @@
 import os
 import uvicorn
 from dotenv import load_dotenv
-from app.api import create_app
 from app.init_db import init_db
 from fastapi import FastAPI
-from app.routers import usuarios
+
+# Importa todos los routers
+from app.routers import usuarios, reservas, reportes, configuracion
 
 # Carga las variables de entorno
 load_dotenv()
@@ -14,7 +15,11 @@ init_db()
 
 app = FastAPI()
 
+# Incluye todos los routers
 app.include_router(usuarios.router, prefix="/api/usuarios", tags=["usuarios"])
+app.include_router(reservas.router, prefix="/api/reservas", tags=["reservas"])
+app.include_router(reportes.router, prefix="/api/reportes", tags=["reportes"])
+app.include_router(configuracion.router, prefix="/api/configuracion", tags=["configuracion"])
 
 if __name__ == "__main__":
     host = os.getenv("HOST", "0.0.0.0")
